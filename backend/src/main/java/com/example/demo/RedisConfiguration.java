9d402b4000827852b5c248df2a25cf401e885ce5
Ahmed El-Qaffas <ahmed.elqaffas97@gmail.com>
2025-12-28 21:06:54 +0200

    use @ConfigurationProperties instead of @Value

--- backend/src/main/java/com/example/demo/RedisConfiguration.java
+++ backend/src/main/java/com/example/demo/RedisConfiguration.java
@@ -1,19 +1,20 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import redis.clients.jedis.RedisClient;
 
 @Configuration
 public class RedisConfiguration {
-    @Autowired
-    private Environment env;
+    private final RedisProperties redisProperties;
+
+    public RedisConfiguration(RedisProperties redisProperties){
+        this.redisProperties = redisProperties;
+    }
 
     @Bean
     public RedisClient redisClient(){
-        return RedisClient.create(env.getProperty("redis.host"),
-                Integer.parseInt(env.getProperty("redis.port")));
+        return RedisClient.create(redisProperties.getHost(),
+                redisProperties.getPort());
     }
 }
